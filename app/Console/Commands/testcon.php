9aca0f7c8ff774c9756f37661f844eeb0632239b
majixian <majixian@lianjia.com>
2016-07-27 13:44:58 +0800

    脚本运行测试

---
+++ app/Console/Commands/testcon.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class testcon extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'testcon';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command used to test console';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+        $this->comment('this is a console test');
+    }
+}
